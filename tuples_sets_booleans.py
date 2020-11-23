3a4bd16e1ec41ed506e7790210a367889e0da27b
Sumit Mandal <63957082+sumit-mandal@users.noreply.github.com>
2020-11-23 19:15:46 +0530

    set

---
+++ tuples_sets_booleans.py
@@ -0,0 +1,23 @@
+# tuples are immutable sequences
+
+t=(1,2,3)
+print("We use close brackets for tuples :",t[0])
+
+t=('a',True,123)
+print(t)
+
+#both tuples and strings are immutable
+
+# sets are unordered collections of unique elements
+
+x=set()
+x.add(1)
+x.add(2)
+x.add(4)
+x.add(4)
+x.add(0.1)
+print("set is :" ,x)
+#even after adding 4 multiple times it will be included only 1 time in final input
+
+
+#Booleans are true ND false
